301a7c89b4b37719f54492504676af022fe805f8
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-18 00:43:44 +0530

    Add project files.

---
+++ Even and Odd/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_and_Odd
+{
+    class Program
+    {
+
+
+
+        static void second()
+        {
+           
+            Console.WriteLine("my name ");
+
+            third();
+        }
+        static void third()
+        {
+            Console.WriteLine(" BCA ");
+            second();
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            second();
+            third();
+
+            Console.ReadLine();
+        }
+
+    }
+}
